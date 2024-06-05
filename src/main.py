0cb35953b90821e28c600feb04254fb4f0597c58
victord03 <vic.kaklamanis@gmail.com>
2024-06-05 11:27:34 +0300

    Worked until base rates. Need to calculate probability sequences.

--- src/main.py
+++ src/main.py
@@ -19,17 +19,37 @@ def calculate_odds(desired_outcomes, total_possible_outcomes) -> float:
     return round(desired_outcomes / total_possible_outcomes, 2)
 
 
-def calculate_expected_value():
-    ...
+def calculate_base_rates(coins) -> dict:
+    base_rates = dict()
+
+    total_number_of_coins = sum(coins.values())
+
+    for coin_value, coin_count in coins.items():
+        base_rates[coin_value] = calculate_odds(coin_count, total_number_of_coins)
+
+    return base_rates
+
+
+'''def calculate_expected_value(coin_values, distribution) -> float:
+    current_odds = float()
+
+    for value, dist in zip(coin_values, distribution):
+        current_odds *= value * dist
+
+    return current_odds'''
 
 
 def main():
-    ...
+
+    coins = {
+        1: 2,
+        2: 4,
+    }
+
+    base_rates = calculate_base_rates(coins)
+
+    print(base_rates)
 
 
 if __name__ == "__main__":
-
-    printing = True
-
-    if printing:
-        print(main())
+    main()
